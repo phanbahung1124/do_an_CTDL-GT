d212209e007da456cfaeca3060dbefbc42b38b4e
phanbahung1124 <115205784+phanbahung1124@users.noreply.github.com>
2023-06-06 22:57:51 +0700

    Update quanma.py

--- chuong 2/quanma.py
+++ chuong 2/quanma.py
@@ -5,8 +5,7 @@ ky_move = [-1, 1, -2, 2, -2, 2, -1, 1]
 
 
 def make_screen(n):
-    # Khởi tạo cửa sổ 400x400 với tên cửa sổ là Result
-    # biến size là đối tượng trong cửa sổ, ta sẽ vẽ lên trên đối tượng này
+    # khởi tạo giao hiện kích thước 400 x 400
     X = 400
     Y = 400
     pygame.init()
@@ -18,7 +17,6 @@ def make_screen(n):
     m = 0
 
     window = True
-    # Vòng lặp vĩnh cửu
     while window:
         for event in pygame.event.get():
             # Khi thoát khỏi cửa sổ, vòng lặp kết thúc
@@ -27,21 +25,17 @@ def make_screen(n):
 
         # Kiểm tra nếu số bước nhỏ hơn số ô cờ
         while m < n*n:
-            pygame.time.wait(500)  # Đợi 0.5s trước khi cập nhật màn hình
-            size.fill((210, 210, 210))  # Lắp đầy màn hình bằng màu xám sáng
-            # Vòng lặp này vẽ các ô vuông màu đen so le với nhau
-            # Kết hợp với màu nền khi nãy, ta có một bàn cờ vua
+            pygame.time.wait(500)
+            size.fill((210, 210, 210))  # màu toàn màn hình
             for i in range(n):
                 for j in range(n):
                     if (i + j) % 2 == 0:
                         pygame.draw.rect(size, (0, 0, 0), pygame.Rect((X / n) * i, (Y / n) * j, X / n, Y / n))
-            # Vẽ các hình tròn màu xanh lá, biểu thị rằng ô cờ đó đã được đi qua
+            # các quân mã hiển thị dưới một hình tròn 
             for k in range(m):
                 pygame.draw.circle(size, (0, 255, 0), ((korder_y[k]+0.5)*(Y/n), (korder_x[k] + 0.5) * (X / n)), X / n / 3)
             # Vẽ hình tròn màu đỏ. biểu thị nước đi hiện tại của quân mã
             pygame.draw.circle(size, (255, 0, 0), ((korder_y[m] + 0.5) * (Y / n), (korder_x[m] + 0.5) * (X / n)), X / n / 3)
-            # Cập nhật màn hình
-            # event.get() tránh tràn bộ nhớ
             pygame.display.flip()
             pygame.event.get()
             # +1 bước
@@ -65,7 +59,7 @@ def move(x, y):
     step += 1 # Tăng số bước lên 1
     chess[y][x] = step
     for i in range(8):  # Lặp lại 8 trường hợp quân mã có thể đi
-        if step == n*n:   # Kiểm tra nếu số bước vượt quá số vị trí của bàn cờ. Nếu có thì kết thúc chương trình
+        if step == n*n:   # kiểm tra số bước vượt quá bàn cờ
             print("Buoc di chuyen cua quan ma:")
             result()
             make_screen(n)
@@ -87,7 +81,7 @@ flag = False
 n = abs(int(input("Nhap so n (ban co nxn): ")))
 
 
-# Khởi tạo số bước là 0 và tạo bàn cờ trống (tất cả các phần tử bằng 0)
+# Khởi tạo số bước là 0 và tạo bàn cờ trống 
 # Hai danh sách dưới dùng để lưu nước đi của quân mã với thứ tự (index) là chỉ số bước di chuyển
 step = 0
 chess = [[0 for i in range(n)] for j in range(n)]
